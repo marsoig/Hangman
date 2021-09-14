e0f3559e106a04bfb5866a2edaf4ea96d4302da5
marsoig <50marsoig@users.noreply.github.com>
2021-09-14 10:34:44 +0300

    It is ready!
    To say nothing for the design

--- script.js
+++ script.js
@@ -2,17 +2,31 @@ let startButton = document.querySelector(".start-button");
 let words = ["бретцель", "достопримечательность", "календарь", "викторина", "артиллерия", "украшение", "неожиданность", "приключение"];
 let word;
 let attemptsLeft = 10;
+let currentWord = 0;
 
 function checkAnswer (answer) {
+
   let rightAnswer = false;
+  let victory = true;
+  let hiddentLetters = document.querySelectorAll(".cell");
+
   for (let i = 0; i < word.length; i++) {
     if (word[i] == answer) {
-      let hiddentLetters = document.querySelectorAll(".cell");
       hiddentLetters[i].firstChild.nodeValue = word[i];
       rightAnswer = true;
     }
   }
+
+  if (rightAnswer == true) {
+    for (let i = 0; i < word.length; i++) {
+      if (hiddentLetters[i].firstChild.nodeValue == '_') {
+        victory = false;
+      }
+    }
+  }
+
   if (rightAnswer == false) {
+    victory == false;
     attemptsLeft -= 1;
     if (attemptsLeft == 0) {
       gameOver();
@@ -23,14 +37,41 @@ function checkAnswer (answer) {
       document.querySelector(".picture").src = "img/" + pictureName + ".png";
     }
   }
+  if (victory == true) { gameOver();}
 }
 
 function gameOver () {
-
+  for (let i = 0; i < word.length; i++) {
+    let hiddentLetters = document.querySelectorAll(".cell");
+    hiddentLetters[i].firstChild.nodeValue = word[i];
+  }
+  let alphabetButtons = document.querySelectorAll(".alphabetLetter");
+  for (let i = 0; i < alphabetButtons.length; i++) {
+    alphabetButtons[i].disabled = true;
+  };
+  let playAgain = document.createElement('button');
+  playAgain.className = 'start-button';
+  let playAgainText = document.createTextNode('Новая игра');
+  playAgain.appendChild(playAgainText);
+  document.querySelector(".picture-container").appendChild(playAgain);
+  playAgain.onclick = function () {
+    let interface = document.querySelector(".interface");
+    while (interface.firstChild) {
+       interface.removeChild(interface.lastChild);
+     }
+    document.querySelector(".picture").src = '';
+    currentWord += 1;
+    if (currentWord > words.length) {currentWord = 0;}
+    startGame();
+  }
 }
 
 startButton.onclick = function () {
-  word = words[6].toUpperCase();
+  startGame();
+};
+
+function startGame() {
+  word = words[currentWord].toUpperCase();
   let rowLetters = document.createElement('div');
   rowLetters.className = 'row-letters';
   for (let i = 0; i < word.length; i++) {
@@ -48,13 +89,15 @@ startButton.onclick = function () {
   let alphabet = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ';
   for (let i = 0; i < alphabet.length; i++) {
      let newLetter = document.createElement('button');
-     //newLetter.className = 'cell';
+     newLetter.className = 'alphabetLetter';
      let newLetterText = document.createTextNode(alphabet[i]);
      newLetter.appendChild(newLetterText);
      letterButtons.appendChild(newLetter);
+
      newLetter.onclick = function () {
        checkAnswer (alphabet[i]);
        this.disabled = true;
+       this.style.textDecoration = 'line-through';
      }
   }
   document.querySelector(".interface").appendChild(letterButtons);
@@ -63,5 +106,5 @@ startButton.onclick = function () {
   pic.className = 'picture';
   document.querySelector(".picture-container").appendChild(pic);
   document.querySelector(".interface").appendChild(letterButtons);
-  startButton.hidden = true;
+  document.querySelector(".start-button").remove();
 }
